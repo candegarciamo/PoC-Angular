f1a6c3955e904c4fee23b19bf15be789defa36e5
Candela Garcia <candegarciamo@gmail.com>
2025-08-11 15:23:06 -0300

    cambio nombres productos

--- mini-store/src/main.ts
+++ mini-store/src/main.ts
@@ -1,8 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http'; // <-- FALTA ESTA
-import { AppComponent } from './app/app.component'; // <-- FALTA ESTA
+import { HttpClientModule } from '@angular/common/http'; 
+import { AppComponent } from './app/app.component'; 
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
