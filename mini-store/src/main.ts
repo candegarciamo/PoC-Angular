086c00b7be2b30112869d19b946e005d14d90256
Candela Garcia <candegarciamo@gmail.com>
2025-08-14 09:53:58 -0300

    correccion httpclient

--- mini-store/src/main.ts
+++ mini-store/src/main.ts
@@ -1,13 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http'; 
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component'; 
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(HttpClientModule),
+    provideHttpClient(),
     provideRouter(routes),
   ],
 });
